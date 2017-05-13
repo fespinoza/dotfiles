08ccfccb1095107c276726f2334f68eff5f8f23a
Felipe Espinoza <fespinozacast@gmail.com>
2017-05-13 13:19:36 +0200

    Improve scripts for varner.

--- zsh/scripts/varner.zsh
+++ zsh/scripts/varner.zsh
@@ -1,10 +1,18 @@
-alias get_file_reports="s3cmd ls s3://myshop-backend-production/uploads/logs/imports/varner/ | grep $(date '+%Y-%m-%d') | sed -n 's_.*\(s3://.*\)_\1_p' | xargs s3cmd get"
-
-alias get_file_reports_staging="s3cmd ls s3://myshop-backend-staging/uploads/logs/imports/varner/ | grep $(date '+%Y-%m-%d') | sed -n 's_.*\(s3://.*\)_\1_p' | xargs s3cmd get"
-
 get_data_files() {
-  files=($(ssh varner-upload@tatooine.hyper.no ls -l | grep $(date '+%Y-%m-%d')))
+  DATE=${1:-$(date '+%Y-%m-%d')}
+  echo "download data files for $DATE"
+  files=($(ssh varner-upload@tatooine.hyper.no ls -l | grep $DATE))
   for file in $files; do
     scp varner-upload@tatooine.hyper.no:$file ~/Downloads/varner
   done
+}
+
+get_file_reports () {
+  DATE=${1:-$(date '+%Y-%m-%d')}
+  SERVER=${2:-production}
+  echo "download file reports for $DATE in $SERVER"
+  s3cmd ls s3://myshop-backend-${SERVER}/uploads/logs/imports/varner/ |
+  grep $DATE |
+  sed -n 's_.*\(s3://.*\)_\1_p' |
+  xargs s3cmd get --skip-existing
 }
